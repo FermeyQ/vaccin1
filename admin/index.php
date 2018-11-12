730909bf23e1cf0e4f82d2c3a0ca90fd279a380d
unknown <hobyfln>
2018-11-12 10:27:43 +0100

    navback

--- admin/index.php
+++ admin/index.php
@@ -3,86 +3,9 @@ include ('inc/headerback.php');
 ?>
 
 <body>
-
-    <div id="wrapper">
-
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">Back-Office</a>
-            </div>
-            <!-- /.navbar-header -->
-
-            <ul class="nav navbar-top-links navbar-right">
-                
-                <!-- /.dropdown -->
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-            </ul>
-            <!-- /.navbar-top-links -->
-
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">  
-                        <li>
-                            <a href="../index.php"><i class="fa fa-edit fa-fw"></i>Clients</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-wrench fa-fw"></i>Users<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="newusers.php">New Users</a>
-                                </li>
-                                <li>
-                                    <a href="editusers.php">Edit Users</a>
-                                </li>
-                                <li>
-                                    <a href="deleteusers.php">Delete Users</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-sitemap fa-fw"></i>Vaccins<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                            <li>
-                                    <a href="newvaccins.php">New Vaccins</a>
-                                </li>
-                                <li>
-                                    <a href="editvaccins.php">Edit Vaccins</a>
-                                </li>
-                                <li>
-                                    <a href="deletevaccins.php">Delete Vaccins</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>
+<?php
+include ('inc/navback.php');
+?>
 
         <!-- Page Content -->
         <div id="page-wrapper">
