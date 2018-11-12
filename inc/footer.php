d3ab05b8caaeb7cd9a58e744bc976b8240081338
hoby <hobyfln@gmail.com>
2018-11-12 11:10:43 +0100

    1

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,45 @@
-    <p>Nous contacter</p>
+<footer>
+  <p>&#169;2018 les vaccins</p>
+
+  <!--FOOTER----------------------------->
+  <footer id="footer">
+    <div class="wrap">
+
+      <!--CASE 1-->
+      <div class="piedpage">
+        <h3>Solutions</h3>
+        <ul>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+        </ul>
+      </div>
+
+      <!--CASE 2--------------------------->
+      <div class="piedpage">
+        <h3>Présentation</h3>
+        <ul>
+          <li><a href="#">les vaccins</a></li>
+          <li><a href="#">mon carnet</a></li>
+        </ul>
+      </div>
+
+      <!--CASE 3--------------------------->
+      <div class="piedpage">
+        <h3>Assistance</h3>
+        <ul>
+          <li><a href="#"></a>Mot de passe oublié</li>
+          <li><a href="#">Informations et presse</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="clear"></div>
+  </footer>
+
+  <a href="#header" id="haut">HAUT DE PAGE</a>
+</footer>
+
+</body>
+</html>
 
   </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -7,17 +7,25 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
   </head>
   <body>
-    <nav>
-      <ul>
-        <li id="header"><a href="admin/index.php">Back-Office</a></li>
-        <li id="header"><a href="index.php">Accueil</li></a>
-        <?php if (isLogged()){ ?>
-        <li id="header"><a href="deconnection.php">Déconnexion</a></li>
-        <!-- <li id="header"><a href="faq.php">(Des questions ?)</li></a> -->
-        <?php }else{ ?>
-        <li id="header"><a href="lesvaccins.php">Les Vaccins</li></a>
-        <li id="header"><a href="inscription.php">Inscription</a></li>
-        <li id="header"><a href="connection.php">Connexion</a></li>     
-        <?php } ?>
- </ul>
-    </nav>
+  <div class="wrap">
+    <header id ="header">
+      <!--La petite bare au dessus-->
+      <div>
+        <h1>VACCINATION</h1>
+      </div>
+      <!--Le menu NAV-->
+      <nav>
+        <ul>
+          <li><a href="index.php">Accueil</li></a>
+          <?php if (isLogged()){ ?>
+          <!-- <li id="header"><a href="faq.php">(Des questions ?)</li></a> -->
+          <?php }else{ ?>
+          <li><a href="lesvaccins.php">Les Vaccins</li></a>
+          <li><a href="moncarnet.php">Mon carnet de vaccination</a></li>
+          <li><a href="connection.php">Connexion</a></li>
+          <li><a href="inscription.php">Inscription</a></li>
+          <?php } ?>
+        </ul>
+      </nav>
+    </header>
+  </div>
